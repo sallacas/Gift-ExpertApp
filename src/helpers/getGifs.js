e11ca578e0d8f470094724e49bc21be0d8b8f3e6
Sallacas <jdcasallas12@gmail.com>
2021-03-08 20:21:15 -0500

    Primer commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifts = async( category ) =>{
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category)}&limit=10&api_key=qvVi9hgm9uHscu0dhamSczjlT6Q09gF0`
+    const resp = await fetch( url );
+    const {data} = await resp.json();
+
+    const gifs = data.map( img => {
+        return{
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    return gifs;
+}
